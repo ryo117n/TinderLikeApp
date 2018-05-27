5b38d4d0f9b09ae2536c5fb7397b99c6cc2247de
ryo117n <ryonakagawa117@gmail.com>
2018-05-28 01:08:05 +0900

    cardを左右に飛ばせるようにした

--- TinderUI/ViewController.swift
+++ TinderUI/ViewController.swift
@@ -13,11 +13,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var basicCard: UIView!
     @IBOutlet weak var likeImageView: UIImageView!
     
+    @IBOutlet weak var person1: UIView!
+    @IBOutlet weak var person2: UIView!
+    @IBOutlet weak var person3: UIView!
+    @IBOutlet weak var person4: UIView!
+    
+    
+    
+    
     var centerOfCard:CGPoint!
+    var people = [UIView]()
+    var selectedCardCount: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         centerOfCard = basicCard.center
+        people.append(person1)
+        people.append(person2)
+        people.append(person3)
+        people.append(person4)
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,14 +39,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func resetCard(){
+        basicCard.center = self.centerOfCard
+        basicCard.transform = .identity
+    }
+    
     @IBAction func swipeCard(_ sender: UIPanGestureRecognizer) {
         let card = sender.view!
         let point =  sender.translation(in:  view)
         
         card.center = CGPoint(x: card.center.x + point.x, y: card.center.y + point.y)
+        people[selectedCardCount].center = CGPoint(x: card.center.x + point.x, y:card.center.y + point.y)
         
+        // swipeした時カードの角度を変える
         let xFromCenter = card.center.x - view.center.x
         card.transform = CGAffineTransform(rotationAngle: xFromCenter / (view.frame.width / 2) * -0.785)
+        people[selectedCardCount].transform = CGAffineTransform(rotationAngle: xFromCenter / (view.frame.width / 2) * -0.785)
         
         if xFromCenter > 0 {
             likeImageView.image = #imageLiteral(resourceName: "good")
@@ -48,12 +70,14 @@ class ViewController: UIViewController {
             //左に大きくスワイプ
             if card.center.x < 75{
                 UIView.animate(withDuration: 0.2, animations: {
-                    card.center = CGPoint(x: card.center.x - 250, y: card.center.y)
+                    self.resetCard()
+                    self.people[self.selectedCardCount].center = CGPoint(x: self.people[self.selectedCardCount].center.x - 250, y: self.people[self.selectedCardCount].center.y)
                 })
                 return
             }else if card.center.x > self.view.frame.width - 75 {
                 UIView.animate(withDuration: 0.2, animations: {
-                    card.center = CGPoint(x: card.center.x + 250, y: card.center.y)
+                    self.resetCard()
+                    self.people[self.selectedCardCount].center = CGPoint(x: self.people[self.selectedCardCount].center.x, y: self.people[self.selectedCardCount].center.y - 250)
                 })
                 return
             }
@@ -61,8 +85,9 @@ class ViewController: UIViewController {
             
             //真ん中に戻る処理
             UIView.animate(withDuration: 0.2, animations: {
-                card.center = self.centerOfCard
-                card.transform = .identity
+                self.resetCard()
+                self.people[self.selectedCardCount].center = self.centerOfCard
+                self.people[self.selectedCardCount].transform = .identity
             })
             likeImageView.alpha = 0
         }
