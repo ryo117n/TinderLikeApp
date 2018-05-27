2c0954005ed8b3cc46815a231fc583f424829362
ryo117n <ryonakagawa117@gmail.com>
2018-05-27 15:33:59 +0900

    Initial Commit

---
+++ TinderUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinderUI
+//
+//  Created by Ryo Nakagawa on 2018/05/27.
+//  Copyright © 2018年 Ryo Nakagawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
