f42a34c5e4ac32e5abbe41e803418fe3bc0b4a2b
ryo117n <ryonakagawa117@gmail.com>
2018-05-28 01:35:08 +0900

    遷移先の画面を作成

---
+++ TinderUI/ListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ListViewController.swift
+//  TinderUI
+//
+//  Created by Ryo Nakagawa on 2018/05/28.
+//  Copyright © 2018年 Ryo Nakagawa. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
